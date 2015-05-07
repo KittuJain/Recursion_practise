6f41828d27233724b13c909e39e8ba65d57eb5e3
kittujain <cskratijain@gmail.com>
2015-05-07 10:46:20 +0530

    Balance of brackets implemented in scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,18 +5,30 @@ object Main {
     for (row <- 0 to 5) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
-        println()
+      println()
     }
   }
 
-
   def pascal(col: Int, row: Int): Int = {
-    var val1 = 0
-    var val2 = 0
-    if (col == 0 || col == row)
-      return 1
-    val1 = pascal(col - 1, row - 1)
-    val2 = pascal(col, row - 1)
-    val1 + val2
+    if (col == 0 || col == row) 1
+    else pascal(col - 1, row - 1) + pascal(col, row - 1)
   }
+
+  def balance(chars: List[Char]): Boolean = {
+    def isBalanced(chars: List[Char], openNumBrac: Int): Boolean = {
+      if (chars.isEmpty) openNumBrac == 0
+      else {
+        val head = chars.head
+        val numOfBrackets =
+          if (head == '(') openNumBrac + 1
+          else if (head == ')') openNumBrac - 1
+          else openNumBrac
+        if (numOfBrackets >= 0) isBalanced(chars.tail, numOfBrackets)
+        else false
+      }
+    }
+    isBalanced(chars, 0)
+  }
+
+
 }
